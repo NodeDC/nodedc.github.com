ce4b5ce6b28f74f033fb61f80fef887f0024ee5e
Will White <will@developmentseed.org>
2011-10-29 19:12:29 -0400

    Factor out two github requests.

--- site.js
+++ site.js
@@ -1,72 +1,79 @@
 ---
 ---
 ;{% include js/jquery-1.6.4.min.js %}
-;{% include js/jquery.jsonp-2.1.4.min.js %}
 ;{% include js/underscore.js %}
-;{% include js/backbone.js %}
 ;
 
 (function(context) {
 
 var longneck = {};
 
-longneck.setup = function() {
+longneck.githubWatcherProject = function(resp) {
+    var watcherProject = $('.follower-project');
+    var i = 0;
+    var max = 4; // Make a maximum of five requests before giving up.
+    var shuffled = _(resp).shuffle();
+
+    var getProjects = function(u) {
+        $.ajax({
+            url: 'https://api.github.com/users/' + u.login + '/repos',
+            dataType: 'json',
+            success: function(resp) {
+                var repo = _(resp)
+                    .chain()
+                    .shuffle()
+                    .detect(function(r) { return r.language === '{{site.github_lanaguage}}' })
+                    .value();
+
+                if (!repo) {
+                    getProjects(shuffled[i++]);
+                } else {
+                    var template =
+                        "<a target='_blank' href='<%=html_url%>'>"
+                        + "<span class='thumb' style='background-image:url(<%=owner.avatar_url%>)'></span>"
+                        + "</a>"
+                        + "<a target='_blank' href='<%=html_url%>'>"
+                        + "<span class='title'><%=owner.login%></span>"
+                        + "</a>"
+                        + "<span class='title'><%=name%></span>"
+                        + "<span class='title'><%=description%></span>"
+                        + "";
+                    var t = _(template).template(repo);
+                    watcherProject.html(t).addClass('loaded');
+                }
+
+            }
+        });
+    };
+    getProjects(shuffled[i]);
+};
+
+longneck.githubWatchers = function() {
     var watchers = $('.followers');
-    var followerProject = $('.follower-project');
-    var tweets = $('.tweets');
-
-
-
     $.ajax({
+        // TODO: this endpoint only returns maximum 30 users. Implement random
+        // pagination so we see different groups of people.
         url: 'https://api.github.com/repos/{{site.github_login}}/{{site.github_repo}}/watchers',
-        dataType: 'jsonp',
+        dataType: 'json',
         success: function(resp) {
-            if (!resp.data.length) return;
+            if (!resp.length) return;
+            longneck.githubWatcherProject(resp);
             var template =
                 "<a class='github-user' target='_blank' href='http://github.com/<%=login%>'>"
                 + "<span style='background-image:url(<%=avatar_url%>)' class='thumb' /></span>"
                 + "</a>";
-            var t = _(resp.data)
-                .chain()
+            var t = _(resp)
                 .map(function(i) { return _(template).template(i); })
-                .value()
                 .join('');
             watchers.html(t);
-            _(resp.data.slice(0,5))
-                .chain()
-                .reject(function(u) { return u.login === '{{site.github_login}}'; })
-                .sortBy(function() { return Math.random() })
-                .each(function(u) {
-                    $.ajax({
-                        url: 'https://api.github.com/users/' + u.login + '/repos',
-                        dataType: 'jsonp',
-                        success: function(resp) {
-                            if (followerProject.hasClass('loaded') || !resp.data.length) return;
-                            var template =
-                                ""
-                                + "<a target='_blank' href='<%=html_url%>'>"
-                                + "<span class='thumb' style='background-image:url(<%=owner.avatar_url%>)'></span>"
-                                + "</a>"
-                                + "<a target='_blank' href='<%=html_url%>'>"
-                                + "<span class='title'><%=owner.login%></span>"
-                                + "</a>"
-                                + "<span class='title'><%=name%></span>"
-                                + "<span class='title'><%=description%></span>"
-                                + "";
-                            var repo = _(resp.data)
-                                .chain()
-                                .sortBy(function() { return Math.random() })
-                                .detect(function(r) { return r.language === '{{site.github_lanaguage}}' })
-                                .value();
-
-                            var t = _(template).template(repo);
-                            followerProject.html(t).addClass('loaded');
-                        }
-                    });
-                });
         }
     });
-    
+};
+$(longneck.githubWatchers);
+
+
+longneck.setup = function() {
+    var tweets = $('.tweets');
 
     $.ajax({
         url: 'http://search.twitter.com/search.json',
